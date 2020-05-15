b21e56bb33ada487bd967ede5c1c59022e0e51ec
SanjitBasker <64987259+SanjitBasker@users.noreply.github.com>
2020-05-15 19:41:00 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,229 +1,100 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-    .select-css {
-	font-size: 16px;
-	font-family: serif;
-	font-weight: normal;
-	color: #444;
-	line-height: 1.3;
-	padding: .6em 1.4em .5em .8em;
-	box-sizing: border-box;
-	margin: 0;
-	border: 1px solid #aaa;
-	box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
-	border-radius: .5em;
-	-moz-appearance: none;
-	-webkit-appearance: none;
-	appearance: none;
-	background-color: #fff;
-	background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
-	  linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
-	background-repeat: no-repeat, repeat;
-	background-position: right .7em top 50%, 0 0;
-	background-size: .65em auto, 100%;
-    }   
-    .select-css::-ms-expand {
-        display: none;
-    }
-    .select-css:hover {
-        border-color: #888;
-    }
-    .select-css:focus {
-        border-color: #aaa;
-        box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
-        box-shadow: 0 0 0 3px -moz-mac-focusring;
-        color: #222;
-        outline: none;
-    }
-    .select-css option {
-        font-weight:normal;
-    }
-    
-    
-    .button {
-      border: none;
-      color: white;
-      padding: 16px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      margin: 4px 2px;
-      transition-duration: 0.4s;
-      cursor: pointer;
-    }
-    .button1 {
-      background-color: white; 
-      color: black; 
-      border: 2px solid #4CAF50;
-    }
-
-    .button1:hover {
-      background-color: #4CAF50;
-      color: white;
-    }
-    
-    /* Customize the label (the container) */
-    .container {
-      display: block;
-      position: relative;
-      padding-left: 35px;
-      margin-bottom: 12px;
-      cursor: pointer;
-      font-size: 22px;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-    }
-
-    /* Hide the browser's default radio button */
-    .container input {
-      position: absolute;
-      opacity: 0;
-      cursor: pointer;
-      height: 0;
-      width: 0;
-    }
-
-    /* Create a custom radio button */
-    .checkmark {
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 25px;
-      width: 25px;
-      background-color: #eee;
-      border-radius: 50%;
-    }
-
-    /* On mouse-over, add a grey background color */
-    .container:hover input ~ .checkmark {
-      background-color: #ccc;
-    }
-
-    /* When the radio button is checked, add a blue background */
-    .container input:checked ~ .checkmark {
-      background-color: #2196F3;
-    }
-
-    /* Create the indicator (the dot/circle - hidden when not checked) */
-    .checkmark:after {
-      content: "";
-      position: absolute;
-      display: none;
-    }
-
-    /* Show the indicator (dot/circle) when checked */
-    .container input:checked ~ .checkmark:after {
-      display: block;
-    }
-
-    /* Style the indicator (dot/circle) */
-    .container .checkmark:after {
-      top: 9px;
-      left: 9px;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-      background: white;
-    }
-
-</style>    
+<link rel="stylesheet" href="../style.css">
     
 <meta charset="UTF-8">
-<title>Title of the document</title>
+<title>Last Letters Quiz</title>
 </head>
 
 <body>
-<table>
-    <tr>
-        <td><p>cold drink</p></td>
-        <td><p>wack</p></td>
-    </tr>
-    <?PHP
-    $questions1 = array("cold drink", "turnip", "hexagon", "truth", "elephant")
-        
-    ?>
+
+
     
-    
-</table>
 <form>
-    <p>cold drink<select name="ans1" class = "select-css">
-        <option>Select below</option>
-        <option>श</option>
-        <option>ष</option>
-        <option>स</option>
-        <option>ह</option>
-    </select></p>
-    <p>turnip<select name="ans2" class = "select-css">
-        <option>Select below</option>
-        <option>श</option>
-        <option>ष</option>
-        <option>स</option>
-        <option>ह</option>
-    </select></p>
-    <p>hexagon<select name="ans3" class = "select-css">
-        <option>Select below</option>
-        <option>श</option>
-        <option>ष</option>
-        <option>स</option>
-        <option>ह</option>
-    </select></p>
-    <p>truth<select name="ans4" class = "select-css">
-        <option>Select below</option>
-        <option>श</option>
-        <option>ष</option>
-        <option>स</option>
-        <option>ह</option>
-    </select></p>
-    <p>elephant<select name="ans5" class = "select-css">
-        <option>Select below</option>
-        <option>श</option>
-        <option>ष</option>
-        <option>स</option>
-        <option>ह</option>
-    </select></p>
-    <p>forgive<select name="ans6" class = "select-css">
-        <option>Select below</option>
-        <option>क्ष</option>
-        <option>त्र</option>
-        <option>ज्ञ</option>
-        <option>श्र</option>
-    </select></p>
-    <p>trident<select name="ans7" class = "select-css">
-        <option>Select below</option>
-        <option>क्ष</option>
-        <option>त्र</option>
-        <option>ज्ञ</option>
-        <option>श्र</option>
-    </select></p>
-    <p>knowledge<select name="ans8" class = "select-css">
-        <option>Select below</option>
-        <option>क्ष</option>
-        <option>त्र</option>
-        <option>ज्ञ</option>
-        <option>श्र</option>
-    </select></p>
-    <br>
-    <button class = "button button1" name="submit" type="submit" value="submit">Calculate</button>
-</form>
+<table>
+    <?PHP
+    $y=0;
+    $questions1 = array("cold drink", "turnip", "hexagon", "truth", "elephant");
+    $choices1=array("श", "ष", "स", "ह");
+    $nums = array(0,1,2,3,4);
+    $nums2 = array(0,1,2,3,4);
+    for ($i = 0; $i < 5; $i++){
+        $y = rand(0,4-$i);
+        $myVar = $nums[$y];
+        
+        echo "<tr><td>";
+            echo "<p class='question'>".$questions1[$myVar]."</p></td>";
+            echo "<td>";
+            for ($j = 0; $j < sizeof($choices1); $j++){
+                echo "<input type='radio' id='".$choices1[$j]."' name='ans".($nums2[$y]+1)."' value='".$choices1[$j],"'><label class='option' for='".$choices1[$j]."'>".$choices1[$j]."</label>";
+            }
+            echo "</td>";
+        echo "</tr>";
+        unset($nums[$y]);
+        unset($nums2[$y]);
+        $nums=array_values($nums);
+        $nums2=array_values($nums2);
+    }
     
+    
+    $y=0;
+    $questions1 = array("forgive", "trident", "knowledge");
+    $choices1=array("क्ष", "त्र", "ज्ञ");
+    $nums = array(0,1,2);
+    $nums2 = array(5,6,7);
+    for ($i = 0; $i < 3; $i++){
+        $y = rand(0,2-$i);
+        $myVar = $nums[$y];
+        
+        echo "<tr><td>";
+            echo "<p class='question'>".$questions1[$myVar]."</p></td>";
+            echo "<td>";
+            for ($j = 0; $j < sizeof($choices1); $j++){
+                echo "<input type='radio' id='".$choices1[$j]."' name='ans".($nums2[$y]+1)."' value='".$choices1[$j],"'><label class='option' for='".$choices1[$j]."'>".$choices1[$j]."</label>";
+            }
+            echo "</td>";
+        echo "</tr>";
+        unset($nums[$y]);
+        unset($nums2[$y]);
+        $nums=array_values($nums);
+        $nums2=array_values($nums2);
+    }
+    echo "</table>";
+    ?>
+</table>
+    <button name="submit" type="submit" value="submit" class = "button button1">Calculate</button>
+</form>
+<br>
 
 <?PHP
-    if (isset($_GET['submit'])){
-        $answers = array($_GET['ans1'],
+    if (isset($_GET['submit'],
+              $_GET['ans1'],
+              $_GET['ans2'],
+              $_GET['ans3'],
+              $_GET['ans4'],
+              $_GET['ans5'],
+              $_GET['ans6'],
+              $_GET['ans7'],
+              $_GET['ans8'],)){
+        $submitted = array($_GET['ans1'],
                          $_GET['ans2'],
                          $_GET['ans3'],
                          $_GET['ans4'],
                          $_GET['ans5'],
                          $_GET['ans6'],
                          $_GET['ans7'],
-                         $_GET['ans8']);
-        $operator =array_intersect($answers,array("श","श","ष","स","ह","क्ष","त्र","ज्ञ"));
-        echo "<p>The score is:</p>".sizeof($operator)."/8";
+                         $_GET['ans8'],
+                        );
+        $answers=array("श","श","ष","स","ह", "क्ष", "त्र", "ज्ञ");
+        $correct = 0;
+        for ($i = 0; $i < sizeof($answers); $i ++){
+            $correct+= ($submitted[$i]==$answers[$i]);
+        }
+        echo "<p>The score is:</p>".$correct."/8";
+    }
+    else if(isset($_GET['submit'])){
+        echo "<p>Some questions not answered.";
     }
 ?>
 
