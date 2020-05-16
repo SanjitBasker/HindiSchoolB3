e71d222d8584af8ee26ab0992b68de57d8095627
SanjitBasker <64987259+SanjitBasker@users.noreply.github.com>
2020-05-16 11:12:08 -0400

    Add files via upload

--- Activities/beginning-vowels/index.php
+++ Activities/beginning-vowels/index.php
@@ -10,7 +10,7 @@
 <body>
 
 
-<h1>Last Letters Activity</h1>
+<h1>Letters Activity</h1>
 <p class='instructions'>
     Select the first letter of each word in Hindi. Use Research material posted in our Google Classroom if you need a reminder for any of the words.</p>
 <form>
@@ -19,14 +19,13 @@
     //initial variables
     $countquestions = 0;
     
-    
 //    first set of questions
     $y=0;
-    $questions1 = array("cold drink", "turnip", "hexagon", "truth", "elephant");
+    $questions1 = array("pomegranate", "pineapple", "mango", "plum", "tamarind");
     $countquestions+=sizeof($questions1);
-    $choices1=array("श", "ष", "स", "ह");
-    $nums = array(0,1,2,3,4);
-    $nums2 = array(0,1,2,3,4);
+    $choices1=array("अ", "आ", "इ");
+    $nums = range(0,sizeof($questions1));
+    $nums2 = range(0,sizeof($questions1));
     for ($i = 0; $i < 5; $i++){
         $y = rand(0,4-$i);
         $myVar = $nums[$y];
@@ -53,38 +52,7 @@
         $nums2=array_values($nums2);
     }
     
-//    second set of questions
-    $y=0;
-    $questions1 = array("forgive", "trident", "knowledge");
-    $countquestions+=sizeof($questions1);
-    $choices1=array("क्ष", "त्र", "ज्ञ");
-    $nums = array(0,1,2);
-    $nums2 = array(5,6,7);
-    for ($i = 0; $i < 3; $i++){
-        $y = rand(0,2-$i);
-        $myVar = $nums[$y];
-        
-        echo "<tr><td>";
-            echo "<p class='question'>".$questions1[$myVar]."</p></td>";
-            echo "<td>";
-            for ($j = 0; $j < sizeof($choices1); $j++){
-                $x="";
-                if (isset($_GET['submit'],$_GET['ans'.($nums2[$y]+1)])){
-                    if($_GET['ans'.($nums2[$y]+1)]==$choices1[$j])
-                    $x = "checked";
-                }
-                else{
-                    $x = "";
-                }
-                echo "<input type='radio' id='".($nums2[$y]+1).'x'.$choices1[$j]."' name='ans".($nums2[$y]+1)."' value='".$choices1[$j]."'".$x."><label class='option' for='".($nums2[$y]+1).'x'.$choices1[$j]."'><span></span>".$choices1[$j]."</label>";
-            }
-            echo "</td>";
-        echo "</tr>";
-        unset($nums[$y]);
-        unset($nums2[$y]);
-        $nums=array_values($nums);
-        $nums2=array_values($nums2);
-    }
+
     echo "</table>";
     ?>
 </table>
@@ -98,7 +66,7 @@
         for ($i = 1; $i<=$countquestions; $i++){
             array_push($submitted, $_GET['ans'.$i]);
         }
-        $answers=array("श","श","ष","स","ह", "क्ष", "त्र", "ज्ञ");
+        $answers=array("अ", "अ", "आ", "आ", "इ");
         $correct = 0;
         for ($i = 0; $i < sizeof($submitted); $i ++){
             $correct+= ($submitted[$i]==$answers[$i]);
